3b423fe40a2875d5f0797c8ed7f48cb7adc96a65
wmollenkopf <biri@biri.me>
2019-02-15 20:30:35 -0500

    Added some basic testing to the project.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const initialCard = {
+  card_id: 0,
+  card_question: '',
+  card_answer: ''
+};
+
+const initialState = {
+  sessionKey:'',
+  allCards: [],
+  card: initialCard,
+  currentCardIndex: 0,
+  showAnswer: false,
+  route: 'signin',
+  isSignedIn: false,
+  editing: false,
+  creating: false,
+  fcardServerURL: `https://fcards-server.biri.me`,
+}
+
+const app = shallow(<App/>);
+describe('App',()=>{
+
+    it('renders correctly', ()=>{
+        expect(app).toMatchSnapshot();
+    });
+    
+    describe('When state is first initialized',()=>{});
+    it('initializes the `state` `fcardServerURL` with a URL',()=>{
+        expect(app.state().fcardServerURL);
+    });
+ 
+
+    
+
+})
+
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+
